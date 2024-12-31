82f6465178b23b8f81849fe4f0e39ec368b560fc
Damon Haase <damon.haase@gmail.com>
2024-12-31 01:01:23 -0500

    Add id

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import Home from "./components/Home";
 import SpinButton from "./components/SpinButton";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
-      <SpinButton />
+      <SpinButton id="test" />
     </>
   );
 }
