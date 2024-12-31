9ccd03e542828b041ceb975c9c4668d96e7ad460
Damon Haase <damon.haase@gmail.com>
2024-12-31 00:56:05 -0500

    Add prop types and min-max values

--- src/components/SpinButton.jsx
+++ src/components/SpinButton.jsx
@@ -1,14 +1,24 @@
-const SpinButton = ({ id, text = "Quantity:" }) => {
+import PropTypes from "prop-types";
+import "../styles/spinButton.css";
+
+const SpinButton = ({ id, text = "Quantity:", min = 0, max = 99 }) => {
   return (
     <div className="spinContainerOuter">
       <label htmlFor={id}>{text}</label>
       <div className="spinContainerInner">
         <button>-</button>
-        <input type="number" name={id} id={id} />
+        <input type="number" name={id} id={id} min={min} max={max} />
         <button>+</button>
       </div>
     </div>
   );
+};
+
+SpinButton.propTypes = {
+  id: PropTypes.string.isRequired,
+  text: PropTypes.string,
+  min: PropTypes.number,
+  max: PropTypes.number,
 };
 
 export default SpinButton;
