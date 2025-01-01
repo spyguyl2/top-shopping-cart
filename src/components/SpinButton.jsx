188da31a8d95ee059daf74e4a2755a26dec22f09
Damon Haase <damon.haase@gmail.com>
2025-01-01 15:19:21 -0500

    SpinButton component is (functionally) complete

--- src/components/SpinButton.jsx
+++ src/components/SpinButton.jsx
@@ -1,14 +1,48 @@
 import PropTypes from "prop-types";
+import { Plus, Minus } from "lucide-react";
 import "../styles/spinButton.css";
+import { useState } from "react";
 
 const SpinButton = ({ id, text = "Quantity:", min = 0, max = 99 }) => {
+  const [quantity, setQuantity] = useState(0);
+
+  const handleIncreaseQuantity = () => {
+    quantity >= max
+      ? setQuantity(max)
+      : setQuantity((prevQuantity) => parseInt(prevQuantity + 1));
+  };
+  const handleDecreaseQuantity = () => {
+    quantity <= min
+      ? setQuantity(min)
+      : setQuantity((prevQuantity) => parseInt(prevQuantity - 1));
+  };
+
+  const handleInputChange = (event) => {
+    let newValue = parseInt(event.target.value);
+    if (newValue <= min) newValue = min;
+    if (newValue >= max) newValue = max;
+    setQuantity(newValue);
+  };
+
   return (
     <div className="spinContainerOuter">
       <label htmlFor={id}>{text}</label>
       <div className="spinContainerInner">
-        <button>-</button>
-        <input type="number" name={id} id={id} min={min} max={max} />
-        <button>+</button>
+        <button aria-label="minus" onClick={handleDecreaseQuantity}>
+          <Minus />
+        </button>
+        <input
+          type="number"
+          name={id}
+          id={id}
+          min={min}
+          max={max}
+          value={quantity}
+          onChange={handleInputChange}
+        />
+        <button aria-label="plus" onClick={handleIncreaseQuantity}>
+          <Plus name="plus" />
+        </button>
       </div>
     </div>
   );
