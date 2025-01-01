4f04324f4b72adb4af17778956d1b304c4545264
Damon Haase <damon.haase@gmail.com>
2025-01-01 15:17:19 -0500

    Begin learning routers

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,18 @@
+import { Link } from "react-router-dom";
 import "../styles/navBar.css";
 
 const NavBar = () => {
   return (
     <div className="navBar">
       <h1>test</h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="shop">Shop</Link>
+        </li>
+      </ul>
       <div className="cartButton">
         <img src="" alt="" />
         <p>Cart:</p>
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,23 @@ import { createRoot } from "react-dom/client";
 import "./styles/reset.css";
 import "./styles/index.css";
 import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { element } from "prop-types";
+import Shop from "./components/Shop.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "shop",
+    element: <Shop />,
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
