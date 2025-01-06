2d1dd13691fda9bab32269d665a2afaafeb1fbbe
Damon Haase <damon.haase@gmail.com>
2025-01-06 10:28:56 -0500

    Finish refactor

--- src/components/SpinButton.jsx
+++ src/components/SpinButton.jsx
@@ -1,6 +1,48 @@
 import PropTypes from "prop-types";
 import { Plus, Minus } from "lucide-react";
-import "../styles/spinButton.css";
+import styled from "styled-components";
+
+const SpinContainerOuter = styled.div`
+  display: flex
+  flex-direction: column;
+  align-items: center;
+  `;
+
+const SpinContainerInner = styled.div`
+  display: flex;
+  height: 2rem;
+  margin-bottom: 0.2rem;
+`;
+
+const StyleButton = styled.button`
+  border-radius: 20px;
+
+  &:hover {
+    cursor: pointer;
+  }
+
+  &:active {
+    background-color: rgb(62, 166, 195);
+    transform: translateY(4px);
+  }
+`;
+
+const StyleSpinInput = styled.input`
+  margin: 0 0.2rem;
+  border-radius: 12px;
+  text-align: center;
+
+  &::-webkit-outer-spin-button,
+  &::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+
+  &[type="number"] {
+    -moz-appearance: textfield;
+    appearance: textfield;
+  }
+`;
 
 const SpinButton = ({
   id,
@@ -15,13 +57,13 @@ const SpinButton = ({
   const handleFocus = (event) => event.target.select();
 
   return (
-    <div className="spinContainerOuter">
+    <SpinContainerOuter>
       <label htmlFor={id}>{text}</label>
-      <div className="spinContainerInner">
-        <button aria-label="minus" onClick={handleDecreaseQuantity}>
+      <SpinContainerInner>
+        <StyleButton aria-label="minus" onClick={handleDecreaseQuantity}>
           <Minus />
-        </button>
-        <input
+        </StyleButton>
+        <StyleSpinInput
           type="number"
           name={id}
           id={id}
@@ -31,11 +73,11 @@ const SpinButton = ({
           onFocus={handleFocus}
           onChange={handleInputChange}
         />
-        <button aria-label="plus" onClick={handleIncreaseQuantity}>
+        <StyleButton aria-label="plus" onClick={handleIncreaseQuantity}>
           <Plus />
-        </button>
-      </div>
-    </div>
+        </StyleButton>
+      </SpinContainerInner>
+    </SpinContainerOuter>
   );
 };
 
