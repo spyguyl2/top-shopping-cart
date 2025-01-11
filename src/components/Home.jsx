913d54101e766e915053d4164c4ada2a9c12541f
Damon Haase <damon.haase@gmail.com>
2025-01-11 14:00:46 -0500

    Remove test button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,8 @@
-import { useOutletContext } from "react-router-dom";
-
 const Home = () => {
-  const { handleToggleSideBar } = useOutletContext();
   return (
     <>
       <h2>This is the home page!</h2>
       <p>You can navigate from here. Yay!</p>
-      <button onClick={handleToggleSideBar}>Toggle</button>
     </>
   );
 };
