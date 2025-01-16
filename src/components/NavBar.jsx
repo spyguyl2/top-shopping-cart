c7f1f24237ff3a9656b6333390fb07ca72f0d3dd
Damon Haase <damon.haase@gmail.com>
2025-01-15 22:27:05 -0500

    Style cart button

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,39 +6,79 @@ import styled from "styled-components";
 const NavBar = ({ cart, openCart }) => {
   return (
     <NavBarWrapper>
-      <h1>test</h1>
-      <ul>
+      <Heading>E-Commerce</Heading>
+      <StyledList>
         <li>
-          <Link to="home">Home</Link>
+          <StyledLink to="home">Home</StyledLink>
         </li>
         <li>
-          <Link to="shop">Shop</Link>
+          <StyledLink to="shop">Shop</StyledLink>
         </li>
-      </ul>
-      <CartButton onClick={openCart}>
-        <ShoppingCart size={48}></ShoppingCart>
-        <p>Checkout: {`(${cart.length})`}</p>
-      </CartButton>
+      </StyledList>
+      <CartCircle onClick={openCart}>
+        <CartButton>
+          <ShoppingCart size={32} />
+        </CartButton>
+        <CartNumber>{cart()}</CartNumber>
+      </CartCircle>
     </NavBarWrapper>
   );
 };
+
+export const Heading = styled.h1`
+  color: ${(props) => props.theme.primary};
+`;
 
 const NavBarWrapper = styled.div`
   display: flex;
   justify-content: space-around;
   align-items: center;
+  margin-top: 0.5rem;
+`;
+
+const CartNumber = styled.h2`
+  position: absolute;
+  color: ${(props) => props.theme.background};
+`;
+
+const CartCircle = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  background-color: ${(props) => props.theme.secondary};
 `;
 
 const CartButton = styled.div`
+  position: relative;
   padding: 1rem;
   cursor: pointer;
+  color: ${(props) => props.theme.primary};
   &:hover {
-    color: aliceblue;
+    color: ${(props) => props.theme.secondary};
   }
 `;
 
+const StyledLink = styled(Link)`
+  color: ${(props) => props.theme.primary};
+  background-color: ${(props) => props.theme.secondary};
+  border: 1px ${(props) => props.theme.tertiary} solid;
+  padding: 0.5rem;
+  border-radius: 24px;
+  font-weight: bold;
+  &:hover {
+    color: ${(props) => props.theme.secondary};
+    background-color: ${(props) => props.theme.primary};
+  }
+`;
+
+const StyledList = styled.ul`
+  display: flex;
+  gap: 2rem;
+`;
+
 NavBar.propTypes = {
-  cart: PropTypes.array.isRequired,
+  cart: PropTypes.func.isRequired,
   openCart: PropTypes.func.isRequired,
 };
 
