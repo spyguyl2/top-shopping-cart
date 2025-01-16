629766e3d7295f773b21d0eba4a11d5ad82a88f2
Damon Haase <damon.haase@gmail.com>
2025-01-15 22:31:00 -0500

    Implement theme. Fix cart quantity

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,22 @@ import { Outlet } from "react-router-dom";
 import { ClickableBackground } from "./styles/styleLibrary";
 import NavBar from "./components/NavBar";
 import SideBar from "./components/SideBar";
-import styled from "styled-components";
+import { styled, ThemeProvider } from "styled-components";
+import theme from "./styles/theme";
 
 function App() {
   const [cart, setCart] = useState([]);
   const [toggleSideBar, setToggleSideBar] = useState(false);
 
   const handleToggleSideBar = () => setToggleSideBar(!toggleSideBar);
+
+  const calcCartQuantity = () => {
+    let total = 0;
+    cart.forEach((item) => {
+      total = item.quantity + total;
+    });
+    return total;
+  };
 
   const handleAddToCart = (shopItem) => {
     if (shopItem.quantity <= 0) return;
@@ -23,22 +32,25 @@ function App() {
   };
 
   return (
-    <Container>
-      <NavBar cart={cart} openCart={handleToggleSideBar} />
-      <Outlet context={{ handleToggleSideBar }} />
-      {toggleSideBar && <ClickableBackground onClick={handleToggleSideBar} />}
-      <SideBar
-        stateToggleOpen={toggleSideBar}
-        onClick={handleToggleSideBar}
-      ></SideBar>
-    </Container>
+    <ThemeProvider theme={theme}>
+      <Container>
+        <NavBar cart={calcCartQuantity} openCart={handleToggleSideBar} />
+        {toggleSideBar && <ClickableBackground onClick={handleToggleSideBar} />}
+        <Outlet context={{ handleAddToCart }} />
+
+        <SideBar
+          stateToggleOpen={toggleSideBar}
+          onClick={handleToggleSideBar}
+        ></SideBar>
+      </Container>
+    </ThemeProvider>
   );
 }
 
 export default App;
 
 const Container = styled.div`
-  background-color: rgb(98, 131, 149);
+  background-color: ${(props) => props.theme.background};
   display: flex;
   flex-direction: column;
   height: 100vh;
